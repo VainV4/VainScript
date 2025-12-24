74c7b0006795e06ff4133fc015c88fc83d482062
Vain <106759694+VainV4@users.noreply.github.com>
2025-12-24 09:20:07 +0100

    Update Config.lua

--- scripts/Config.lua
+++ scripts/Config.lua
@@ -1,11 +1,10 @@
--- Config.lua
-local Config = {}
+local Config = _G.Vain.Config
 
 Config.Settings = {
-	METAL_ESP = { ENABLED = false, COLOR = Color3.fromRGB(0, 170, 255) },
-	BEE_ESP   = { ENABLED = false, COLOR = Color3.fromRGB(255, 214, 6) },
-	STAR_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255, 255, 255) },
-	TREE_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255, 100, 0) },
+	METAL_ESP = { ENABLED = false, COLOR = Color3.fromRGB(0,170,255) },
+	BEE_ESP   = { ENABLED = false, COLOR = Color3.fromRGB(255,214,6) },
+	STAR_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255,255,255) },
+	TREE_ESP  = { ENABLED = false, COLOR = Color3.fromRGB(255,100,0) },
 
 	AIM_ASSIST = {
 		ENABLED = false,
@@ -14,19 +13,15 @@ Config.Settings = {
 		MAX_ANGLE = 90
 	},
 
-	UI_COLOR = Color3.fromRGB(12, 12, 14),
+	UI_COLOR = Color3.fromRGB(12,12,14),
 	KEY_UI_TOGGLE = Enum.KeyCode.RightShift,
 	KEY_AIM_TOGGLE = Enum.KeyCode.Q,
 	VISIBLE = true
 }
 
--- Gemeinsamer ESP-State
 Config.ActiveObjects = {
 	metal = {},
-	star  = {},
-	tree  = {},
-	bee   = {}
+	star = {},
+	tree = {},
+	bee = {}
 }
-
-return Config
-
